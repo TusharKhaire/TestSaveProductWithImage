e6e9fbe4805ea07010f7c535f7586f8b4a39327b
Tushar Khaire <tusharkhaire099@gmail.com>
2024-11-26 20:19:15 +0530

    Add project files.

---
+++ Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Product
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Decimal Price { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public byte[] Image { get; set; }
+
+    }
+}
---
+++ Services/Product_Services.cs
@@ -0,0 +1,18 @@
+﻿using IRepository;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class Product_Services : IProduct
+    {
+        public Task<bool> SaveProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
